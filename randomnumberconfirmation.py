ee68c20af0fc989ae9ce5f9b31cd1b760b743035
LadyAudrey <105085549+LadyAudrey@users.noreply.github.com>
2022-05-30 12:09:18 -0700

    Delete randomnumberconfirmation.py

--- randomnumberconfirmation.py
+++
@@ -1,7 +0,0 @@
-# random number generator and then testing that the numbers are random
-import random
-
-low = input("What is your lowest number?")
-high = input("What is your highest number?")
-sample = random.randint(low,high):
-print (sample)
